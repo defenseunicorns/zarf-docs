ff6d64e2bf83f1496463633bbf95c79ea21f9dd7
Jon Perry <yrrepnoj@gmail.com>
2022-05-24 12:34:39 -0400

    make default style dark-mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,6 +37,9 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark',
+      },
       navbar: {
         title: 'Zarf Docs',
         logo: {
